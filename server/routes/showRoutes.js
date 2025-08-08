7a3f329e5ddbcce2416c9af84c28fbbf2fdb7143
Saduni Amarasekara <saduvindhaya@gmail.com>
2025-08-08 23:13:31 +0530

    showroutes

--- server/routes/showRoutes.js
+++ server/routes/showRoutes.js
@@ -5,11 +5,12 @@ import {
   getShow,
   getShows,
 } from "../controllers/showController.js";
+import { protectAdmin } from "../middleware/auth.js";
 
 const showRouter = express.Router();
 
-showRouter.get("/now-playing", getNowPlayingMovies);
-showRouter.post("/add", addShow);
+showRouter.get("/now-playing",protectAdmin, getNowPlayingMovies);
+showRouter.post("/add",protectAdmin, addShow);
 
 // Use GET for fetching all shows
 showRouter.get("/all", getShows);
