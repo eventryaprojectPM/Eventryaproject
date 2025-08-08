789902f2aa973332eab04267a1136bd9c51dd863
Saduni Amarasekara <saduvindhaya@gmail.com>
2025-08-08 13:18:57 +0530

    showRoutes

--- server/routes/showRoutes.js
+++ server/routes/showRoutes.js
@@ -1,10 +1,20 @@
 import express from "express";
-import { addShow, getNowPlayingMovies } from "../controllers/showController.js";
-import { protectAdmin } from "../middleware/auth.js";
+import {
+  getNowPlayingMovies,
+  addShow,
+  getShow,
+  getShows,
+} from "../controllers/showController.js";
 
 const showRouter = express.Router();
 
-showRouter.get("/now-playing",protectAdmin, getNowPlayingMovies);
-showRouter.post('/add',protectAdmin, addShow)
+showRouter.get("/now-playing", getNowPlayingMovies);
+showRouter.post("/add", addShow);
+
+// Use GET for fetching all shows
+showRouter.get("/all", getShows);
+
+// Use GET with movieId param to get show details
+showRouter.get("/:movieId", getShow);
 
 export default showRouter;
