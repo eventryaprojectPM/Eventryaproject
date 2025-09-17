d1e77f7961653441d9fe218a977fcf01ff880da6
Saduni Amarasekara <saduvindhaya@gmail.com>
2025-09-17 19:23:06 +0530

    adminnavbar

--- client/src/Components/admin/AdminNavbar.jsx
+++ client/src/Components/admin/AdminNavbar.jsx
@@ -5,7 +5,7 @@ const AdminNavbar = () => {
   return (
     <div className="flex items-center justify-between px-6 md:px-10 h-16 border-b border-gray-300/30">
       <Link to="/">
-        <img src="/images/logo1.png" alt="Admin Logo" className="w-36 h-auto" />
+        <img src="/logo.jpg" alt="" className="w-10 h-10 object-cover rounded-full border-2 border-white shadow-md" />
       </Link>
     </div>
   );
